c544c4d005fe0c32ef3d4e49349af0125d853045
firecoded <yepyepguru@gmail.com>
2019-01-16 15:06:41 -0800

    added basic checkbox logic, working on styling

--- src/SingleListItem.js
+++ src/SingleListItem.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./singleListItem.css";
 
 class SingleListItem extends Component {
-    
+    state = {
+        isChecked: false
+    }
   
+    handleClick = (e) => {
+        if(this.props.disableEdit){
+            return;
+        }
+        this.setState({
+            isChecked: !this.state.isChecked
+        })
+    }
+
     render() {
         return (
             <li>
-                <div>
-                    {this.props.content}
+                <div className = "list-item">
+                    <label className = "checkbox-container">
+                    <input type = "checkbox" 
+                           onChange = {this.handleClick} 
+                           checked = {this.state.isChecked ? 'checked' : ""}
+                           />
+                    <span className = "checkmark"></span>
+                    </label>
+                    <p className = {this.state.isChecked ? "grayed-out-text" : ""}>
+                        {this.props.content}
+                    </p>
                 </div>
             </li>
         )
